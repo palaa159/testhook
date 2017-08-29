d00d55fe0b13e055c919d619b7f52307e6051b76
Apon Palanuwech <palaa159@newschool.edu>
2017-08-30 00:13:18 +0700

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,6 +1,8 @@
 <?php
+	echo "testing"
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 	if ( $_POST['payload'] ) {
+		echo "received hook";
   		shell_exec( 'git reset --hard HEAD && git pull origin master' );
 	}
 ?>
